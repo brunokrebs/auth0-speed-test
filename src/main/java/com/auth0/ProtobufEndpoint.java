dc93860ca8a2a9602e0671e016446f8f322e4bf2
bruno.krebs <krebs.bruno@gmail.com>
2017-01-19 15:34:32 -0300

    Trying to use protobuf serialization

--- src/main/java/com/auth0/ProtobufEndpoint.java
+++ src/main/java/com/auth0/ProtobufEndpoint.java
@@ -1,14 +1,17 @@
 package com.auth0;
 
+import com.auth0.protobuf.Entities;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class ProtoBufferEndpoint {
+public class ProtobufEndpoint {
 
     @RequestMapping(path = "/proto-buffer-endpoint", method= RequestMethod.GET)
-    public Person getPerson() {
-        return new Person("Bruno", 32,182, 82, null);
+    public List<Entities.Person> getPerson() {
+        return SpeedTestApp.getPeople();
     }
 }
--- src/main/java/com/auth0/SpeedTestApp.java
+++ src/main/java/com/auth0/SpeedTestApp.java
@@ -1,11 +1,56 @@
 package com.auth0;
 
+import com.auth0.protobuf.Entities;
+import com.google.protobuf.InvalidProtocolBufferException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class SpeedTestApp {
-    public static void main(String[] args) {
+    private static List<Entities.Person> people = new ArrayList<>();
+
+    @Configuration
+    public static class RestClientConfiguration {
+
+        @Bean
+        RestTemplate restTemplate(ProtobufHttpMessageConverter hmc) {
+            return new RestTemplate(Arrays.asList(hmc));
+        }
+
+        @Bean
+        ProtobufHttpMessageConverter protobufHttpMessageConverter() {
+            return new ProtobufHttpMessageConverter();
+        }
+    }
+
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        for (int i = 0; i < 5000; i++) {
+            Entities.Address.Builder addressBuilder = Entities.Address.newBuilder();
+            addressBuilder.setStreet("Street Number " + i);
+            addressBuilder.setNumber(i);
+
+            Entities.Person.Builder personBuilder = Entities.Person.newBuilder();
+            personBuilder.setName("Person Number " + i);
+            personBuilder.setAge(32);
+            personBuilder.setHeight(182);
+            personBuilder.setWeight(82);
+            personBuilder.setAddress(addressBuilder);
+
+            people.add(personBuilder.build());
+        }
+
         SpringApplication.run(SpeedTestApp.class, args);
+    }
+
+    public static List<Entities.Person> getPeople() {
+        return people;
     }
 }
