293dbca8ef9970393afed71334613452dd8708ea
bruno.krebs <krebs.bruno@gmail.com>
2017-01-20 19:34:41 -0300

    Java parsing protobuf.

--- src/main/java/com/auth0/ProtobufEndpoint.java
+++ src/main/java/com/auth0/ProtobufEndpoint.java
@@ -1,9 +1,16 @@
 package com.auth0;
 
+import com.auth0.protobuf.PeopleOuterClass;
 import com.auth0.protobuf.PeopleOuterClass.People;
+import com.google.protobuf.util.JsonFormat;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Date;
 
 @RestController
 public class ProtobufEndpoint {
@@ -16,5 +23,20 @@ public class ProtobufEndpoint {
     @RequestMapping(path = "/json/people", method= RequestMethod.GET, produces = "application/json")
     public People getPeopleJSON() {
         return SpeedTestApp.getPeople();
+    }
+
+    @RequestMapping(path = "/protobuf/java", method= RequestMethod.GET)
+    public void getProtobufJava() throws UnirestException, IOException {
+        long start = (new Date()).getTime();
+        People people = People.parseFrom(Unirest.get("http://localhost:8080/protobuf/people").asBinary().getRawBody());
+        System.out.println(people.getPersonList().size());
+        long time = (new Date()).getTime() - start;
+        System.out.println("It took " + time + "ms to load with protobuf.");
+
+        start = (new Date()).getTime();
+        JsonFormat.parser().merge(Unirest.get("http://localhost:8080/json/people").asString().getBody(), People.newBuilder());
+        System.out.println(people.getPersonList().size());
+        time = (new Date()).getTime() - start;
+        System.out.println("It took " + time + "ms to load with json.");
     }
 }
